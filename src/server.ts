9fcc533603cb60383c9af422c7d46d55ad88310e
Md. Ruhul Amin <dev.ruhulamin3@gmail.com>
2023-05-29 15:56:42 +0600

    mongoose express cors setup done

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+
+import mongoose from "mongoose";
+import { app } from "./app";
+import config from "./config/index";
+
+async function connectDb() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("database connected successfully");
+    app.listen(config.port, () => {
+      console.log("server running on port", config.port);
+    });
+  } catch (err) {
+    console.log("failed to connect database");
+  }
+}
+
+connectDb(); 
